35e6a9d12886beee25b99d608499e3e20eb7214d
ItzYald <ibutov07@mail.ru>
2024-12-30 17:49:11 +0300

    Добавлены ограничения на передачи

--- Scenes/Garage/RatioLabel.cs
+++ Scenes/Garage/RatioLabel.cs
@@ -1,17 +1,20 @@
 using Godot;
 using NewCar.Models;
+using StreetRacing.Models;
 using System;
 
 public partial class RatioLabel : Label
 {
 	HSlider slider;
 	int number;
+	Transmission playerTransmission;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		slider = GetNode<HSlider>("RatioSlider");
-        slider.Value = 100 / 4f * MainModel.gameplayModel.playerCar.specifications.transmission.ratios[number];
+		playerTransmission = MainModel.gameplayModel.playerCar.specifications.transmission;
+        slider = GetNode<HSlider>("RatioSlider");
+        slider.Value = 100 / 4f * playerTransmission.ratios[number];
     }
 
 	public void SetNumber(int number)
@@ -22,8 +25,24 @@ public partial class RatioLabel : Label
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Text = (number + 1).ToString() + " - " + MainModel.gameplayModel.playerCar.specifications.transmission.ratios[number];
-		MainModel.gameplayModel.playerCar.specifications.transmission.ratios[number] = (float)slider.Value * 4f / 100f;
+		Text = (number + 1).ToString() + " - " + playerTransmission.ratios[number];
+        playerTransmission.ratios[number] = (float)slider.Value * 4f / 100f;
+
+		if (number != 0)
+			if (playerTransmission.ratios[number]
+				>= playerTransmission.ratios[number - 1])
+			{
+				playerTransmission.ratios[number] = playerTransmission.ratios[number - 1];
+			    slider.Value = 100 / 4f * playerTransmission.ratios[number];
+			}
+
+		if (number != playerTransmission.ratios.Count - 1)
+			if (playerTransmission.ratios[number]
+			    <= playerTransmission.ratios[number + 1])
+			{
+			    playerTransmission.ratios[number] = playerTransmission.ratios[number + 1];
+			    slider.Value = 100 / 4f * playerTransmission.ratios[number];
+			}
 
     }
 }
